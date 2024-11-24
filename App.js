308c95381c7e72986bc7b611936dd792fc761d3e
Chamodi Welmilla <chamodiwelmilla99@gmail.com>
2024-11-24 11:10:28 +0530

    feature/tw

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex-1 items-center justify-center bg-white`}>
+      <Text style={tw`text-red-500`}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
